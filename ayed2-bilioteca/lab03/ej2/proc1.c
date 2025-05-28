00501bdfa2c020427183f55a432117982fdb08af
Agustin <82238717+AgU131@users.noreply.github.com>
2025-05-28 13:27:21 -0300

    Add files via upload

---
+++ ayed2-bilioteca/lab03/ej2/proc1.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void absolute(int x, int y) {
+    // Completar aquí
+
+    if(x>0){
+        y=x;
+    } else{
+        y=-x;
+    }
+    
+}
+
+
+int main(void) {
+    int a=0, res=0;
+    //
+    // Completar aquí
+    //
+    return EXIT_SUCCESS;
+}
+
